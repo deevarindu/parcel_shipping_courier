ac988640903a3fd43c019c3e33e9d58210a2307c
deevarindu <deevarindu@gmail.com>
2022-03-13 01:31:01 +0700

    finish checkout screen

--- lib/app.dart
+++ lib/app.dart
@@ -14,7 +14,7 @@ class App extends StatelessWidget {
       title: 'Parcel Shipping Courier App',
       theme: ParcelAppTheme.lightTheme,
       home: Scaffold(
-        body: SendParcelDetailScreen(),
+        body: SendParcelCheckoutScreen(),
         bottomNavigationBar: MyBottomNavigationBar(),
       ),
     );
